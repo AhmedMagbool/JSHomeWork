a36f995da01290224aa619579fc3c24e23f69a47
ahmed <aalageely@gmail.com>
2023-08-10 07:21:55 +0300

    day4 hw

---
+++ day4/main.js
@@ -0,0 +1,24 @@
+//task1
+let num = 42;
+let numtostr = String(num);
+let str = "25";
+let strtonum = Number(str);
+//task2
+let str_2 = "10";
+let str2tonum = Number(str_2);
+let a = (20)+(str2tonum);
+let b = (20)-(str2tonum);
+//task3
+let str_3 = "15.5";
+let str3tofloat = parseFloat(str_3);
+let c = (str3tofloat)*(2);
+let d = (str3tofloat)/(2);
+//task4
+let str_4 = "5";
+let num_4 = 3;
+let strplusnum = (str_4)+(num_4);
+//task5
+let bool = true;
+let num_5 = 1
+let boolplusnum = (bool)+(num_5)
+//end HW4
