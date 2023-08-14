4851d8fc9ba7ed0a8d38a50dae75d46249429859
ahmed <aalageely@gmail.com>
2023-08-14 10:43:11 +0300

    day6

---
+++ day6/main.js
@@ -0,0 +1,13 @@
+//task1
+let names = ["محمد","فهد","خالد","سماح","علي"]
+//task2
+console.log(names[2]);
+//task3
+names.unshift("احمد");
+//task4
+names.push("ريان");
+//task5
+names.shift();
+//task6
+names.pop();
+//end
