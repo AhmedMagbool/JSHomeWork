d988208e1f3dea2322bedbce70e2d8ae38312627
ahmed <aalageely@gmail.com>
2023-08-17 14:13:55 +0300

    day9

---
+++ day9/main.js
@@ -0,0 +1,30 @@
+//task1
+
+for(let a = 1; a <= 30; a++){
+    if (a <= 10) {
+     alert(a);   
+    }
+}
+
+// task2
+
+for(let b = 1; b <= 20; b++){
+ if(b != 10){
+alert(b)
+ }
+}
+
+//task3
+let c = 3;
+while ( c >= 1) {
+    alert(c);
+    c--;
+}
+
+//task4
+let d = 0
+do {
+   alert(d);
+   d++ 
+} while (d <= 5);
+//end
