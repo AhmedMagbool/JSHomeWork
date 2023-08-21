2beaf3f0cfdf6b0ccc36535ce1677aecf312d3d8
ahmed <aalageely@gmail.com>
2023-08-21 14:29:22 +0300

    func_hw

---
+++ HW-Function/main.js
@@ -0,0 +1,17 @@
+//task1
+function javascript_Boot_camp(teach) {
+alert("This javaScript BootCamp with"+ teach+ "ok");
+}
+javascript_Boot_camp("Teacher1 ")
+javascript_Boot_camp("Teacher2 ")
+//task2
+function avr(num_1,num_2) {
+    let cal = num_1 + num_2;
+    alert(cal / 2)
+}
+
+avr(parseInt(prompt()),parseInt(prompt()));
+
+avr(parseFloat(prompt()),parseFloat(prompt()));
+
+//end
